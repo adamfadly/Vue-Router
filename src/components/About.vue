1bf701e5442f521374d441b78da9dad27d8d0a4f
adam fadly <adamfadly11@gmail.com>
2019-11-28 16:53:07 +0700

    Register Route Vue

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>{{ title }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      title: 'lorem5'
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
