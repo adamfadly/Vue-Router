25555057c4a51d7ccacabbb94324a06ea1189081
Adam Fadly <adamfadly11@gmail.com>
2019-12-01 13:39:01 +0700

    create update user_id on route

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
+import ViewProfile from '@/components/ViewProfile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/About',
-      name: 'About',
-      component: About
-    }
+    { path: '/', name: 'Home', component: Home }, 
+    { path: '/about', name: 'About', component: About },
+    { path: '/profile/:user_id', name: 'ViewProfile', component: ViewProfile }
   ]
 })
